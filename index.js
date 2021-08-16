bc2e6d9f4702c6a100f016bdc81042d71642e6a8
Luke Cole <Lukeajcole@gmail.com>
2021-08-16 16:11:29 -0700

    HTML Generator is functioning; need to fix links and build test

--- index.js
+++ index.js
@@ -3,29 +3,37 @@
 // THEN an HTML file is generated that displays a nicely formatted team roster based on user input
 // const htmlObj = fs.readFile('./assets/htmlJSON.JSON');
 const fs = require('fs');
+const inquirer = require('inquirer');
 const HTML = require('./assets/htmlObj.js');
 const htmlObj = new HTML();
-console.log(htmlObj.htmlLit);
-const testhtml = htmlObj.htmlLit.join();
-fs.writeFile('testHTML.html',testhtml,(err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-  });
+
+initManager();
 
 
-// fs.readFile('./assets/htmlJSON.JSON', 'utf8', (err, jsonString) => {
-//     if (err) {
-//         console.log("Error reading file from disk:", err)
-//         return
-//     }
-//     try {
-//         htmlObj = JSON.parse(jsonString);
-//         htmlMaker(htmlObj);
-//         // console.log(htmlObj);
-// } catch(err) {
-//         console.log('Error parsing JSON string:', err)
-//     }
-// })
+
+function buildSite(data){
+  let cards =[]
+  for (i=0;i<data.team.length;i++){
+      var newCard = data.team[i].makeCard()
+      cards.push(newCard);
+  }
+
+  htmlObj.htmlLit[1] = data.teamName + 'Team Roster';
+  htmlObj.htmlLit[3] = data.teamName; 
+  htmlObj.htmlLit[5] = data.teamDescription;
+
+
+  //Insert the Cards into the greater HTML use Spread to put the whole array in the right spot. 
+  htmlObj.htmlLit.splice(7,0,...cards)
+  //Join the array into one string
+  const siteHTML = htmlObj.htmlLit.join(" ");
+
+  fs.writeFile('testHTML.html',siteHTML,(err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+    });
+}
+
 
 // WHEN I click on an email address in the HTML
 // THEN my default email program opens and populates the TO field of the email with the address
@@ -48,6 +56,23 @@ class Manager extends Employee{
       super(name,id,email)
       this.office = office;
     }
+    makeCard(){
+     const html = `
+      <div class = 'd-flex flex-col '>
+        <div class="card m-3" style="width: 18rem;">
+          <div class="card-header">
+            ${this.name}
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">${this.id}</li>
+            <li class="list-group-item"><a href="mailto:${this.email}">${this.email}</a></li>
+            <li class="list-group-item">${this.office}</li>
+          </ul>
+        </div>
+      </div>`
+      
+      return html;
+    }
 }
 
 
@@ -58,6 +83,23 @@ class Engineer extends Employee{
         super(name,id,email)
         this.github = github;
       }
+    makeCard(){
+      const html = 
+      `<div class = 'd-flex flex-col '>
+        <div class="card m-3" style="width: 18rem;">
+          <div class="card-header">
+            ${this.name}
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${this.id}</li>
+            <li class="list-group-item">EMAIL: <a href="mailto:${this.email}">${this.email}</a></li>
+            <li class="list-group-item">GITHUB: <a href="github.com/${this.github}"</a></li>
+          </ul>
+        </div>
+      </div>`
+      
+      return html;
+    }
 }
 
 // THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
@@ -66,8 +108,161 @@ class Intern extends Employee{
         super(name,id,email)
         this.school = school;
       }
+    makeCard(){
+      const html = 
+      `<div class = 'd-flex flex-col '>
+        <div class="card m-3" style="width: 18rem;">
+          <div class="card-header">
+            ${this.name}
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${this.id}</li>
+            <li class="list-group-item">EMAIL" <a href="mailto:${this.email}">${this.email}</a></li>
+            <li class="list-group-item">SCHOOL: ${this.school}</li>
+          </ul>
+        </div>
+      </div>`
+      
+      return html;
+    }
 }
 
 
 // WHEN I decide to finish building my team
 // THEN I exit the application, and the HTML is generated
+// team manager’s name, employee ID, email address, and office number
+// WHEN I enter the team manager’s name, employee ID, email address, and office number
+
+function initManager(){
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "teamName",
+        message: "What is the Team Name"
+      },
+      {
+        type: "input",
+        name: "teamDescription",
+        message: "What does this team do?"
+      },
+      {
+        type: "input",
+        name: "managerName",
+        message: "Who is the Team Manager?"
+      },
+      {
+        type: "input",
+        name: "managerId",
+        message: "What is his/her employee ID?"
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "What is their email address?"
+      },
+      {
+        type: "input",
+        name: "managerOffice",
+        message: "What is their office number?"
+      }
+    ])
+    .then(val => {
+      manager = new Manager(val.managerId, val.managerEmail, val.managerOffice);
+
+      const siteData = {
+        teamName: val.teamName,
+        teamDescription: val.teamDescription,
+        team: [manager]
+      }
+      console.log(siteData);
+      menu(siteData);
+    })
+    .catch(err => console.log(err));
+}
+
+function menu(data){
+  inquirer
+    .prompt([{
+      type: "list",
+      name: "nextEmployee",
+      message: "Add another employee...",
+      choices: ['Intern', 'Engineer','All Done!']
+    }
+  ])
+  .then(
+    val => {
+      const selection = val.nextEmployee;
+      if (selection == 'Intern')  addIntern(data);
+      if (selection == 'Engineer') addEngineer(data);
+      if (selection == 'All Done!') buildSite(data);
+    }
+  )
+  .catch(err => console.log(err));
+};
+
+
+function addEngineer(data){
+  inquirer
+    .prompt([
+      {
+          type: "input",
+          name: "engineerName",
+          message: "What is the Engineer's name?"
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "What is their employee ID?"
+      },
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is their email address?"
+      },
+      {
+        type: "input",
+        name: "engineerGithub",
+        message: "What is their github username?"
+      }
+    ])
+    .then(val => {
+      console.log(val);
+      const engineer = new Engineer(val.engineerName, val.engineerId, val.engineerEmail, val.engineerGithub)
+      data.team.push(engineer);
+      menu(data);
+    })
+    .catch(err => console.log(err));
+}
+function addIntern(data){
+  inquirer
+    .prompt([
+      {
+          type: "input",
+          name: "internName",
+          message: "What is the Intern's name?"
+      },
+      {
+        type: "input",
+        name: "internId",
+        message: "What is their employee ID?"
+      },
+      {
+        type: "input",
+        name: "internEmail",
+        message: "What is their email address?"
+      },
+      {
+        type: "input",
+        name: "internSchool",
+        message: "What is their school name?"
+      }
+    ])
+    .then(val => {
+      console.log(val);
+      const intern = new Intern(val.internName, val.internId, val.internEmail, val.internSchool)
+      data.team.push(intern);
+      menu(data);
+    })
+    .catch(err => console.log(err));
+}
