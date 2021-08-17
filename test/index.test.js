6952990bd0497fedeaf50a764a3cb1985fced558
Luke Cole <Lukeajcole@gmail.com>
2021-08-16 21:23:44 -0700

    Improve Index Main Test

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,36 @@
 const Index = require('../index.js');
 const HTML = require('../lib/htmlObj')
+const Engineer = require("../lib/engineer.js")
+const Intern = require("../lib/intern.js")
+const Manager = require("../lib/manager.js");
+const Builder = require('../index.js');
 
-describe("Index Main", () =>{
-   it('should just work', () => {
-       expect(1).toBe(1);
-   })
-    
+describe("HTML OBJ", () =>{
+   
+    it('should have the html template as an array of 8 strings', () => {
+        const html = new HTML();
+       expect(html.htmlLit.length).toBe(8);
+   });    
+});
+
+describe("BUILDER BuildSite", () =>{
+   
+    it('should have the html template as an array of 11 strings after getting 3 users', () => {
+        const html = new HTML();
+        const sampleInput =
+        {
+            teamName: 'TEST',
+            teamDescription: 'TEST',
+            team: [
+              new Manager('TEST', 'TEST','TEST', 'TEST' ),
+              new Intern ('TEST', 'TEST','TEST', 'TEST' ),
+              new Engineer('TEST', 'TEST','TEST', 'TEST' )
+            ]
+          }
+        
+         const build = new Builder();
+         build.siteData = sampleInput;
+         build.buildSite();
+       expect(build.htmlLit.length).toBe(10);
+   });    
 });
